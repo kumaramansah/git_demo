422274ac04f6ffd888fe4bb253c40d3918b9c347
Aman Kumar <amanmjh10@gmail.com>
2025-02-15 16:10:27 +0530

    modified user adding getter and setter

--- src/main/java/com/example/git/model/User.java
+++ src/main/java/com/example/git/model/User.java
@@ -4,4 +4,28 @@ public class User {
     private int id;
     private String email;
     private String user_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
